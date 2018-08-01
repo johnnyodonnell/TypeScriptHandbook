84c51a81899c01c9fb78b677329d5f38620fd690
Johnny <odonnell_johnny@yahoo.com>
2018-08-01 16:12:01 -0700

    Add explanation to interfacesExtendingClasses example

--- interfacesExtendingClasses.ts
+++ interfacesExtendingClasses.ts
@@ -23,3 +23,18 @@ class Component implements SelectableControl {
     private state: any;
     select() { }
 }
+
+/*
+Explanation (https://www.typescriptlang.org/docs/handbook/classes.html):
+
+TypeScript is a structural type system. When we compare two different types,
+regardless of where they came from, if the types of all members are compatible,
+then we say the types themselves are compatible.
+
+However, when comparing types that have private and protected members,
+we treat these types differently. For two types to be considered compatible,
+if one of them has a private member, then the other must have a private
+member that originated in the same declaration. The same applies to
+protected members.
+
+*/
