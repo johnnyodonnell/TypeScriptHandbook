b6a682b19f92e2014b65ef950b1e3c96fb79f4c5
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-08-22 15:58:13 -0700

    Add TypeScript compiler as part of npm package
    Add example showing that generics are implicitly polymorphic, unlike in
    Java

---
+++ genericsPolymorphic.ts
@@ -0,0 +1,41 @@
+
+/*
+
+This isn't from the TypeScript Handbook,
+but I wanted to explore this behavior after
+reading the StackOverflow question listed
+below:
+
+https://stackoverflow.com/questions/2745265/is-listdog-a-subclass-of-listanimal-why-are-java-generics-not-implicitly-po
+
+*/
+
+class MyList<T> {
+    arr: T[] = [];
+
+    push(item: T) {
+        this.arr.push(item);
+    }
+
+    length() {
+        return this.arr.length;
+    }
+
+    at(index: number) {
+        return this.arr[index];
+    }
+};
+
+const func = (arr: MyList<any>) => {
+    arr.push("This is a string");
+};
+
+let arr = new MyList<number>();
+arr.push(1);
+arr.push(2);
+arr.push(4);
+func(arr);
+
+for (let i = 0; i < arr.length(); ++i) {
+    console.log(arr.at(i));
+}
